6a43b457078c99d81363f3e8c795c2f0ea768849
kimberly ong <e0424807@u.nus.edu>
2021-08-17 17:31:42 +0800

    refactor: remove console log

--- server/fileRouter.js
+++ server/fileRouter.js
@@ -1,19 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
-let count = 0
+
 router.post("/upload", (req, res) => {
-  console.log(new Date());
-  console.log(count);
-  count++;
   res.status(200).send({
-    
     id: Date.now(),
   });
 });
 
-router.get('/:id', (req, res) => {
-  res.sendFile('doge.jpeg', { root: process.cwd() })
-})
+router.get("/:id", (req, res) => {
+  res.sendFile("doge.jpeg", { root: process.cwd() });
+});
 
 module.exports = router;
