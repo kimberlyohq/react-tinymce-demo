fb03c70c470b0397920a8bbf16370975f36c1f8f
kimberly ong <e0424807@u.nus.edu>
2021-08-19 17:59:38 +0800

    chore: remove console log

--- src/ImageUpload/useLazyLoad.js
+++ src/ImageUpload/useLazyLoad.js
@@ -2,41 +2,40 @@ import { useEffect } from "react";
 import "intersection-observer";
 import { fetchInlineImage } from "./utils";
 
-export const useLazyLoad = (editor, { root = null, threshold = 0 }) => {
+const defaultConfig = { root: null, threshold: 0 };
+
+export const useLazyLoad = (editor, config = defaultConfig) => {
   useEffect(() => {
     if (editor) {
       const externalImages = Array.from(editor.dom.select("img[data-src]"));
 
       const inlineImages = Array.from(editor.dom.select("img[data-cid]"));
 
-      const observer = new IntersectionObserver(
-        (entries, observer) => {
-          entries.forEach((entry) => {
-            if (entry.intersectionRatio <= 0.3) return;
-            const node = entry.target;
-            // already loaded
-            if (!!node.getAttribute("src")) return;
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio <= 0.3) return;
 
-            // external image
-            if (node.getAttribute("data-src")) {
-              const dataSrc = node.getAttribute("data-src");
-              node.removeAttribute("data-src");
-              node.setAttribute("src", dataSrc);
-              node.setAttribute("data-mce-src", dataSrc);
-              observer.unobserve(node);
-            } else {
-              // inline images
-              fetchInlineImage(node, node.getAttribute("data-cid"));
-              observer.unobserve(node);
-            }
-          });
-        },
-        { root: null, threshold: 1.0 }
-      );
+          const node = entry.target;
+          // already loaded
+          if (!!node.getAttribute("src")) return;
+
+          // external images
+          if (node.getAttribute("data-src")) {
+            const dataSrc = node.getAttribute("data-src");
+            node.removeAttribute("data-src");
+            node.setAttribute("src", dataSrc);
+            node.setAttribute("data-mce-src", dataSrc);
+            observer.unobserve(node);
+          } else {
+            // inline images
+            fetchInlineImage(node, node.getAttribute("data-cid"));
+            observer.unobserve(node);
+          }
+        });
+      }, config);
 
       // set up observer for each image
       [...externalImages, ...inlineImages].forEach((node) => {
-        console.log("here");
         observer.observe(node);
       });
 
